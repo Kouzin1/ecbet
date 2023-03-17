0a45f53ee19a9f844aacb6a8ffbab22083127ecc
Akinwunmi Samuel <saakinwunmi@student.oauife.edu.ng>
2023-03-17 01:06:47 +0100

    Sandbox Update

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -4,7 +4,10 @@ import { client } from '../src/services/redis';
 const run = async () => {
     await client.hSet('car', {
         color: 'red',
-        year: 1950
+        year: 1950,
+        engine: { cylinders: 8},
+        owner: null || '',
+        service: undefined || ''
     });
 
     const car = await client.hGetAll('car');

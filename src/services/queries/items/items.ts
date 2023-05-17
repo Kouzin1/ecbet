696201655ff8af39eca1f71310cd951d283c5c9f
Akinwunmi Samuel <saakinwunmi@student.oauife.edu.ng>
2023-05-17 22:49:43 +0100

    Ending time

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsByEndingAtKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -26,6 +26,7 @@ export const getItems = async (ids: string[]) => {
         if (Object.keys(result).length === 0) {
             return null;
         }
+
         return deserialize(ids[i], result)
     });
 };
@@ -38,8 +39,12 @@ export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
     await Promise.all([
         client.hSet(itemsKey(id), serialized),
         client.zAdd(itemsByViewsKey(), {
-            value: id,
+            value: id, 
             score: 0
+        }),
+        client.zAdd(itemsByEndingAtKey(), {
+            value: id,
+            score: attrs.endingAt.toMillis()
         })
     ])
 
